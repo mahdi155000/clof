bc09481e225c7ade0752e0d6b89824301ac43d19
mahdi <mahdi155000@gmail.com>
2025-12-18 00:54:45 +0330

    add update and add interactive functions

--- main.c
+++ main.c
@@ -86,66 +86,130 @@ int movie_exists(const char *title, int season, int episode) {
 }
 
 // Update the watched status of a movie
-void update_watched(const char *title, int watched){
+void update_watched(const char *title, int season, int episode, int watched){
 	for (int i = 0; i < movie_count; i++){
-		if (strcmp(movies[i].title, title) == 0) {
+		if (strcmp(movies[i].title, title) == 0 &&
+			movies[i].season == season &&
+			movies[i].episode == episode) {
+
 			movies[i].watched = watched;
-			printf("Updated '%s' to watched = %d\n", title, watched);
+			printf("Updated '%s' S%02dE%02d ->  watched = %d\n", title, season, episode, watched);
 			return; // stop searching after update
 		}
 	}
 	printf("Movie '%s' not found\n", title); 
 }
 
-int main(int argc, char *argv[]) {
-	char new_title[100];
-	int season, episode, watched;
-	char watched_udpate[10];
+void show_menu() {
+	printf("\n===== LOF Menu =====\n");
+    printf("1. Add movie / episode\n");
+    printf("2. List movies\n");
+    printf("3. Update watched status\n");
+    printf("4. Save and exit\n");
+    printf("====================\n");
+    printf("Choose an option: ");
+}
 
-	load_movies();
 
-	while(1) {
-		printf("Enter new movie title (or 'quit' to exit): ");
-		fgets(new_title, sizeof(new_title), stdin);
-		new_title[strcspn(new_title, "\n")] == 0; //remove newline
+void add_movie_interactive() {
+	char title[100];
+	int is_series;
+	int season = 0;
+	int episode = 0;
+	int watched;
 
-	if (strcmp(new_title, "quit") == 0) {
-		break; // exit loop
-	}
+	printf("Enter title: ");
+	fgets(title, sizeof(title), stdin);
+	title[strcspn(title, "\n")] = 0;
 
-	printf("Is this a series? (0 = No, i = Yes): ");
-	scanf("%d", &season);
-	getchar();	// consume leftover newline
+	printf("Is this a series? (0 = No ,1 = Yes): ");
+	scanf("%d", &is_series);
+	getchar(); // consume newline
 
-	if (season == 1) {
-		// Series: Ask for season and episode
+	if ( is_series == 1) {
 		printf("Enter season number: ");
 		scanf("%d", &season);
+
 		printf("Enter episode number: ");
 		scanf("%d", &episode);
 		getchar();
-	} else {
-		season = 0;
-		episode = 0;
 	}
 
-	if (movie_exists(new_title, season, episode)) {
-		printf("Movie '%s' already exists!\n", new_title);
-		continue;
+	if (movie_exists(title,season,episode)) {
+		printf("This movie/episode is already exists!\n");
+		return;
 	}
-	
-	// Ask watched status
-	printf("Have you watched it? (0 = No, 1 = Yes): ");
+
+	printf("Have you watched it? (0 = No, 1 = Yes)");
 	scanf("%d", &watched);
 	getchar();
 
-	add_movie(new_title,season,episode,watched);
-	printf("Added '%s'\n", new_title);
-	list_movies();
-
-	save_movies();
-
-	return 0;
-	}
+	add_movie(title,season,episode,watched);
+	printf("Movie added successfuly.\n");
 }
 
+void update_movie_interactive() {
+	char title[100];
+	int season;
+	int episode;
+	int watched;
+
+	printf("Enter title to update: ");
+	fgets(title, sizeof(title), stdin);
+	title[strcspn(title, "\n")] = 0;
+
+	printf("Enter season number (0 if movie): ");
+	scanf("%d", &season);
+
+	printf("Enter episode number (0 if movie): ");
+	scanf("%d", &episode);
+	getchar();
+
+	if (!movie_exists(title,season,episode)) {
+		printf("Movie/episode not found!\n");
+		return;
+	}
+
+	printf("Enter new watched status (0 = No, 1 = Yes): ");
+	scanf("%d", &watched);
+	getchar();
+
+
+	update_watched(title,season,episode,watched);
+
+
+}
+
+int main(int argc, char *argv[]) {
+	int choice;
+
+	printf("Welcome to clof\n");
+
+	load_movies();
+
+	while (1) {
+		show_menu();
+
+		scanf("%d", &choice);
+		getchar(); // consume newline
+
+		if (choice == 1) {
+			add_movie_interactive();
+		}
+		else if ( choice == 2 ) {
+			list_movies();
+		}
+		else if ( choice == 3 ) {
+			update_movie_interactive();
+		}
+		else if ( choice == 4 ) {
+			save_movies();
+			printf("Goodbye!\n");
+			break;
+		}
+		else {
+			printf("Invalid option!\n");
+		}
+	}
+	return 0;
+}
