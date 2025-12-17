47218dceb18d0b8a22823f404bb2e68337936d87
mahdi <mahdi155000@gmail.com>
2025-12-17 16:47:25 +0330

    add input to get the information from user

--- main.c
+++ main.c
@@ -7,17 +7,54 @@ typedef struct {
 	int watched;	// 0 for no and 1 for yes
 } Movie;
 
+#define MAX_MOVIES 100
+Movie movies[MAX_MOVIES];
+int movie_count =0;
+
+
+void add_movie(const char *title, int watched) {
+	if (movie_count < MAX_MOVIES) {
+		strcpy(movies[movie_count].title, title);
+		movies[movie_count].watched = watched;
+		movie_count++;
+	} else {
+		printf("Movie database full!\n");
+	}
+}
+
+
+void list_movies(){
+	if (movie_count == 0) {
+		printf("Ther is no movies in database.\n");
+		return;
+	}
+
+	for (int i = 0;i  < movie_count; i++){
+		printf("%d: %s (watched: %d)\n", i+1, movies[i].title, movies[i].watched);
+	}
+
+}
+
 int main(int argc, char *argv[]) {
-    printf("clof started\n");
-	Movie myMovie;
+	char title[100];
+	int watched;
 
-	strcpy(myMovie.title, "Breaking Bad S01E01");
+	printf("clof started\n");
 
-	myMovie.watched = 0;
+//    add_movie("Breaking bad S01E01", 0);
+//    add_movie("Super Natural S01E01", 1);
+	printf("Enter movie title: ");
+	fgets(title, sizeof(title), stdin);
+	title[strcspn(title, "\n")] = 0;
 
-	printf("Movie: %s\n", myMovie.title);
-	printf("Watched: %d\n", myMovie.watched);
+	printf("Have you watched it? (0 = No, 1 = Yes): ");
+	scanf("%d" ,&watched);
+	getchar();
 
-    return 0;
+	add_movie(title, watched);
+
+	list_movies();
+
+	return 0;
 }
 
