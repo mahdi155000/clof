c8c8dcb453d0282738ab5edcda60680f4e069682
mahdi <mahdi155000@gmail.com>
2025-12-18 19:45:06 +0330

    add arrow navigation

--- plugins/tui.c
+++ plugins/tui.c
@@ -1,50 +1,69 @@
 #include <ncurses.h>
 #include "../movie.h"
 
-static void draw_lof_screen(void);
+static void draw_lof_screen(int selected){
+    clear();
+
+    mvprintw(0,0, "clof (ncurses)");
+    mvprintw(1,0, "Use ↑ ↓ to move, q to quit");
+
+    int row = 3;
+
+    for (int i = 0; i < movie_count; i++){
+        if (i == selected)
+            attron(A_REVERSE);
+
+        if (movies[i].is_series) {
+            mvprintw(row, 0,
+                "%2d) %-20s S%02dE%02d",
+                i + 1,
+                movies[i].title,
+                movies[i].season,
+                movies[i].episode);
+        } else {
+            mvprintw(row, 0,
+                "%2d) %-20s (movie)",
+                i + 1,
+                movies[i].title);
+        }
+
+        if ( i == selected)
+            attroff(A_REVERSE);
+        
+        row ++;
+    }
+    
+    refresh();
+}
 
 void plugin_tui(void) {
+    int ch;
+    int selected = 0;
+
     // initialize ncurces
     initscr();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
+    curs_set(0);    // hide cursor
 
-    draw_lof_screen();
-    refresh();
+    while (1){
+        draw_lof_screen(selected);
+        ch = getch();
 
-    // wait until user press 'q'
-    int ch;
-    while ((ch = getch()) != 'q') {
-        // pass just for now
+        if (ch == 'q') {
+            break;
+        } else if (ch == KEY_UP)
+        {
+            if (selected > 0)
+                selected--;
+        } else if (ch == KEY_DOWN)
+        {
+            if (selected < movie_count -1)
+                selected++;
+        }
     }
 
     // restore terminal
     endwin();
-}
-
-static void draw_lof_screen(void) {
-    clear();
-
-    mvprintw(0, 0, "LOF (ncurses)");
-    mvprintw(1, 0, "Press q to quit");
-
-    int row = 3;
-
-    for (int i = 0; i < movie_count; i++) {
-        if (movies[i].is_series) {
-            mvprintw(row, 0,
-                     "%2d) %-20s S%02dE%02d",
-                     i + 1,
-                     movies[i].title,
-                     movies[i].season,
-                     movies[i].episode);
-        } else {
-            mvprintw(row, 0,
-                     "%2d) %-20s (movie)",
-                     i + 1,
-                     movies[i].title);
-        }
-        row++;
-    }
 }
