964f681f28c439c3cf8a0c2763fd4357c5df8203
mahdi <mahdi155000@gmail.com>
2025-12-18 13:42:47 +0330

    write update plugin

---
+++ plugins/update.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../movie.h"
+
+void plugin_update(void) {
+    int input;
+    int index;
+
+    printf("Entry number (+n / -N): ");
+    if (scanf("%d", &input) != 1) {
+        getchar();
+        return;
+    }
+
+    getchar();
+
+    index = abs(input) - 1;
+
+    if (index < 0 || index >= movie_count) {
+        printf("Invalid index.]n");
+        return;
+    }
+
+    if (!movies[index].is_series) {
+        printf("Not a series.\n");
+        return;
+    }
+
+    if (input > 0 ) 
+        next_episode(index);
+    else
+        prev_episode(index);
+    
+    save_movies();
+
+    printf("Updated: %s S%02dE%02d\n",
+            movies[index].title,
+                movies[index].season,
+            movies[index].episode);
+}
